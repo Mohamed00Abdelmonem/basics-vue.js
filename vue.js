9e098a44ee4c2fe441fa5f7c60debac0ff75e3dd
unknown <mmohamedabdelm@gmail.com>
2024-01-23 13:19:27 +0200

    add function for counter

--- vue.js
+++ vue.js
@@ -2,14 +2,21 @@ const app = Vue.createApp({
     data(){
         return{ 
         name:"Mohamed Abdelmonme",
-        length: null
+        length: null,
+        counter: 0,
         };
     },
     methods: {
         myfunction(){
             this.length = this.name.length;
             return this.length
-        }
+        },
+
+        counter1(){
+            ;
+            return this.counter+=1
+        },
+
     },
 }).mount('#app');
 
