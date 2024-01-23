7f914bcd05068b25eda439fd3a52f8d15216dbec
unknown <mmohamedabdelm@gmail.com>
2024-01-23 13:13:43 +0200

    add function for length you name

--- vue.js
+++ vue.js
@@ -1,8 +1,15 @@
 const app = Vue.createApp({
     data(){
         return{ 
-        name:"Mohamed Abdelmonme"
+        name:"Mohamed Abdelmonme",
+        length: null
         };
+    },
+    methods: {
+        myfunction(){
+            this.length = this.name.length;
+            return this.length
+        }
     },
 }).mount('#app');
 
